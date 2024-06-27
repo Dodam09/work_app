865148e93b8998a99c6943a466db86a570d85a7b
Yunki09 <126834731+Dodam09@users.noreply.github.com>
2024-06-27 14:55:12 +0900

    네번째 커밋

---
+++ .next/static/zOCeV44jn59eIOdNzBq4x/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/948-c5968dfa70ca8f25.js","static/chunks/pages/index-1e1ef63764d4b5aa.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- README.md
+++ README.md
@@ -39,3 +39,5 @@ The easiest way to deploy your Next.js app is to use the [Vercel Platform](https
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
 # work_app
+# work_app # README.md 파일에 내용 추가
+# work_app
